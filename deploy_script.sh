e9d43531d304694d3460ec6dc808a8431b5ba7c9
gchang12 <gc8@hawaii.edu>
2023-02-19 21:57:20 -0800

    Initial commit

---
+++ deploy_script.sh
@@ -0,0 +1,8 @@
+git init
+heroku create
+pip install gunicorn psycopg2
+pip freeze > requirements.txt
+echo "web: gunicorn main:app" > Procfile
+git add .; git commit -a -m "Initial commit"
+git push heroku master
+heroku open
