0618d783162936b225f9c685f7f6083467337b77
George Chang <59218296+gchang12@users.noreply.github.com>
2023-02-19 21:53:25 -0800

    initial commit
    Works fine from local repo

---
+++ app.py
@@ -0,0 +1,16 @@
+"""
+Prints epoch-time as a string, and displays
+it on the Flask application.
+"""
+from time import time as get_epoch_time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """
+    Prints epoch-time on home-page.
+    """
+    return str(get_epoch_time())
